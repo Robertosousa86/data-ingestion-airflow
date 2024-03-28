ba90316bd2ca11c6e2e9ec422173dac31ede1868
Roberto Sousa <robertosousa186@gmail.com>
2024-03-28 00:30:28 -0300

    Add indicators to database

--- dags/stocks_dag.py
+++ dags/stocks_dag.py
@@ -5,7 +5,9 @@ from airflow.providers.common.sql.sensors.sql import SqlSensor
 from airflow.models import Connection
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
+import pandas as pd
 import psycopg2
+import sqlalchemy
 import requests
 
 polygon_key = Variable.get("polygon_key")
@@ -18,9 +20,9 @@ def request_data():
         yesterday = today - timedelta(days=7)
         date = yesterday.strftime("%Y-%m-%d")
 
-        response = requests.get(
-            f"https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/{date}?adjusted=true&apiKey={polygon_key}"
-        )
+        url = f"https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/{date}?adjusted=true&apiKey={polygon_key}"
+
+        response = requests.get(url)
 
         result = response.json()
 
@@ -77,6 +79,95 @@ def insert_data():
         raise SystemExit(e)
 
 
+def create_dataframe():
+    conn = create_connection()
+
+    query = """ SELECT ticker, close_price, window_end_timestamp FROM stocks"""
+
+    df = pd.read_sql_query(query, conn)
+
+    return df
+
+
+def caculate_smv(df, window=20):
+    simple_moving_average = df["close_price"].rolling(window).mean()
+
+    return simple_moving_average
+
+
+def calculate_rsi(df, periods=14, ema=True):
+    close_delta = df["close_price"].diff()
+
+    up = close_delta.clip(lower=0)
+    down = -1 * close_delta.clip(upper=0)
+
+    if ema == True:
+        ma_up = up.ewm(com=periods - 1, adjust=True, min_periods=periods).mean()
+        ma_down = down.ewm(com=periods - 1, adjust=True, min_periods=periods).mean()
+    else:
+        ma_up = up.rolling(window=periods, adjust=False).mean()
+        ma_down = down.rolling(window=periods, adjust=False).mean()
+
+    relative_strength_index = ma_up / ma_down
+    relative_strength_index = 100 - (100 / (1 + relative_strength_index))
+
+    return relative_strength_index
+
+
+def calculate_bollinger_bands(df, window=20, num_std=2):
+
+    rolling_mean = df["close_price"].rolling(window=window).mean()
+
+    rolling_std = df["close_price"].rolling(window=window).std()
+
+    upper_band = rolling_mean + (rolling_std * num_std)
+    lower_band = rolling_mean - (rolling_std * num_std)
+
+    return upper_band, lower_band
+
+
+def add_indicators_to_dataframe():
+    df = create_dataframe()
+
+    df["SMV"] = caculate_smv(df)
+
+    df["RSI"] = calculate_rsi(df)
+
+    upper_band, lower_band = calculate_bollinger_bands(df)
+    df["Upper_Band"] = upper_band
+    df["Lower_Band"] = lower_band
+
+    return df
+
+
+def indicators_to_database():
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    new_df = add_indicators_to_dataframe()
+
+    for index, row in new_df.iterrows():
+        cursor.execute(
+            """
+            INSERT INTO stocks_indicators (ticker, window_end_timestamp, simple_moving_average, 
+            relative_strength_index, upper_bollinger_band, lower_bollinger_band, calculated_at)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """,
+            (
+                row["ticker"],
+                row["window_end_timestamp"],
+                row["SMV"],
+                row["RSI"],
+                row["Upper_Band"],
+                row["Lower_Band"],
+                datetime.now(),
+            ),
+        )
+
+    conn.commit()
+    cursor.close()
+
+
 default_args = {
     "owner": "Roberto Sousa",
     "start_date": datetime(2024, 3, 21),
@@ -112,9 +203,15 @@ wait_for_postgres = SqlSensor(
 )
 
 save_data = PythonOperator(
-    task_id="task_2",
+    task_id="insert_data",
     python_callable=insert_data,
     dag=dag,
 )
 
-wait_for_service >> wait_for_postgres >> save_data
+add_indicators = PythonOperator(
+    task_id="insert_indicators",
+    python_callable=indicators_to_database,
+    dag=dag,
+)
+
+wait_for_service >> wait_for_postgres >> save_data >> add_indicators
